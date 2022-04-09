a822652453eab0b245dd0fb43e23827405161896
rahul-6472 <rahulpandey0663@gmail.com>
2022-04-09 14:08:42 +0530

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-let screen = document.getElementById("screen")
-buttons = document.querySelectorAll("button")
-let screenValue =  ""
+let screen = document.querySelector("#screen")
+
+let screenValue = ""
+
+let buttons = document.querySelectorAll("button")
 
 for (item of buttons){
-    item.addEventListener("click", (ele) => {
-       let buttonText = ele.target.innerText
 
-        if (buttonText == "×"){
+    item.addEventListener("click", (ele) => {
+        let buttonText = ele.target.innerText
+
+        if (buttonText == "×") {
             buttonText = "*"
             screenValue += buttonText
             screen.value = screenValue
         }
-
         else if (buttonText == "C"){
             screenValue = ""
             screen.value = screenValue
         }
         else if (buttonText == "="){
-            screen.value = eval(screenValue)
+            screen.value = eval (screenValue)
         }
         else{
-            screenValue += buttonText
+            screenValue += buttonText 
             screen.value = screenValue
         }
     })
